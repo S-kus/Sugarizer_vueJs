3229361a0b03b2dac2fdbd304b2193aa8d8c1c29
S-kus <saumyakus@gmail.com>
2022-09-22 11:59:30 +0530

    initial setup for buttonIcon

---
+++ js/buttonIcon.js
@@ -0,0 +1,23 @@
+const ButtonIcon ={
+    name: 'ButtonIcon',
+    template: `<div class="iconButton">
+                <button-icon
+                    :id=this.id
+                    :svgfile=this.svgfile 
+                    :color=this.color 
+                    :size=this.size
+                    :x=this.x
+                    :y=this.y
+                ></button-icon>
+                <p class="icon-button-text">{{ this.text }}</p>
+            </div>`,
+	props: ['text','id','svgfile','color','size','x','y'],
+    components: {
+        'button-icon': Icon, 
+    },
+    data() {
+        return {
+            
+        }
+    },
+};
