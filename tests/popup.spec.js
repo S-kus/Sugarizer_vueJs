c301b540e901a69d4f887bb48803f6e2606a6a99
S-kus <saumyakus@gmail.com>
2022-10-20 01:24:08 +0530

    added test for absent header in item prop

--- tests/popup.spec.js
+++ tests/popup.spec.js
@@ -118,7 +118,7 @@ describe('Popup.vue', () => {
 		await wrapper.vm.show(100,100);
 		expect(wrapper.find('.home-activity-popup').exists()).toBe(true);
 		expect(wrapper.find('#items').exists()).toBe(true);
-
+		// itemList not present
 		item2= {
 			id: "5",
 			icon: { id: "11", iconData:  svgfile2, color: "95", size: "30"},
@@ -137,7 +137,7 @@ describe('Popup.vue', () => {
 		expect(wrapper.find('.home-activity-popup').exists()).toBe(true);
 		expect(wrapper.find('#items').exists()).toBe(false);
 		expect(wrapper.find('#footer').exists()).toBe(true);
-
+		// footerList not present
 		item2= {
 			id: "5",
 			icon: { id: "11", iconData:  svgfile2, color: "95", size: "30"},
@@ -159,7 +159,49 @@ describe('Popup.vue', () => {
 		expect(wrapper.find('#footer').exists()).toBe(false);
 	});
 
+	it('should render a empty component if item header data is null when passed', async () => {
+		await wrapper.vm.show(100,100);
+		expect(wrapper.find('.home-activity-popup').exists()).toBe(true);
+		// name in the header is not present
+		item2= {
+			id: "5",
+			icon: { id: "11", iconData:  svgfile2, color: "95", size: "30"},
+			title: "Write Activity",
+			itemList: [
+				{ icon: { id: "12", iconData:  svgfile2, color: "95", size: "20" }, name: "item1" },
+				{ icon: { id: "13", iconData:  svgfile2, color: "95", size: "20" }, name: "item2" }
+			]
+		}
+
+		wrapper = mount(Popup, {
+			props: { 
+				item: item2
+			},
+		})
+		await wrapper.vm.show(100,100);
+		expect(wrapper.find('.home-activity-popup').exists()).toBe(false);
+		// icon in the header is not present
+		item2= {
+			id: "5",
+			name: "Write",
+			title: "Write Activity",
+			itemList: [
+				{ icon: { id: "12", iconData:  svgfile2, color: "95", size: "20" }, name: "item1" },
+				{ icon: { id: "13", iconData:  svgfile2, color: "95", size: "20" }, name: "item2" }
+			],
+			footerList: [
+				{ icon: { id: "14", iconData:  svgfile2, color: "1024", size: "20" }, name: "footer1" },
+			],
+		}
+		wrapper = mount(Popup, {
+			props: { 
+				item: item2
+			},
+		})
+		await wrapper.vm.show(100,100);
+		expect(wrapper.find('.home-activity-popup').exists()).toBe(false);
+	});
+
 	// TODO: 1. Test for isCursorInside function, this test should get the component and screen dimentions
 	//		 2. Test for update hook of the dom, to correct the position of popup based on it's width and screen size
-	//		 3. Test when item header is null
 })
