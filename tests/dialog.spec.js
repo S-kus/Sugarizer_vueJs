87a3b3389ed49e5ae7008cffb8780731b4547c5e
S-kus <saumyakus@gmail.com>
2023-01-31 01:17:45 +0530

    corrected indentation to tabs

--- tests/dialog.spec.js
+++ tests/dialog.spec.js
@@ -15,16 +15,16 @@ describe('Dialog.vue', () => {
 	const svgfile="file://"+filename+"\\../icons/owner-icon.svg" ;
 
 	it('renders props when passed', () => {
-        wrapper = mount(Dialog, {
+		wrapper = mount(Dialog, {
 			props: { 
-                searchField: 'true',
-                okButton: 'false',
-                cancelButton: 'true',
-                iconData: svgfile,
-                titleData: text
+				searchField: 'true',
+				okButton: 'false',
+				cancelButton: 'true',
+				iconData: svgfile,
+				titleData: text
 			},
 		})
-        
+		
 		expect(wrapper.props('searchField')).toBe('true');
 		expect(wrapper.props('okButton')).toBe('false');
 		expect(wrapper.props('cancelButton')).toBe('true');
@@ -32,110 +32,110 @@ describe('Dialog.vue', () => {
 		expect(wrapper.props('titleData')).toBe(text);
 	});
 
-    it('onOk emitted on clicking ok button when passed', async () => {
-        wrapper = mount(Dialog, {
+	it('onOk emitted on clicking ok button when passed', async () => {
+		wrapper = mount(Dialog, {
 			props: { 
-                searchField: 'true',
-                okButton: 'false',
-                cancelButton: 'true',
-                iconData: svgfile,
-                titleData: text
+				searchField: 'true',
+				okButton: 'false',
+				cancelButton: 'true',
+				iconData: svgfile,
+				titleData: text
 			},
 		})
-        await wrapper.setData({showDialog: true});
-        expect(wrapper.find('.module-ok-button').exists()).toBe(false)
+		await wrapper.setData({showDialog: true});
+		expect(wrapper.find('.module-ok-button').exists()).toBe(false)
 
-        wrapper = mount(Dialog, {
+		wrapper = mount(Dialog, {
 			props: { 
-                searchField: 'true',
-                okButton: 'true',
-                cancelButton: 'true',
-                iconData: svgfile,
-                titleData: text
+				searchField: 'true',
+				okButton: 'true',
+				cancelButton: 'true',
+				iconData: svgfile,
+				titleData: text
 			},
 		})
-        await wrapper.setData({showDialog: true});
-        expect(wrapper.find('.module-ok-button').exists()).toBe(true)
+		await wrapper.setData({showDialog: true});
+		expect(wrapper.find('.module-ok-button').exists()).toBe(true)
 
-        await wrapper.find('.module-ok-button').trigger('click')
-        expect(wrapper.emitted()).toHaveProperty('onOk')
+		await wrapper.find('.module-ok-button').trigger('click')
+		expect(wrapper.emitted()).toHaveProperty('onOk')
 	});
 
-    it('onCancel emitted on clicking cancel button when passed', async () => {
-        wrapper = mount(Dialog, {
+	it('onCancel emitted on clicking cancel button when passed', async () => {
+		wrapper = mount(Dialog, {
 			props: { 
-                searchField: 'true',
-                okButton: 'true',
-                cancelButton: 'false',
-                iconData: svgfile,
-                titleData: text
+				searchField: 'true',
+				okButton: 'true',
+				cancelButton: 'false',
+				iconData: svgfile,
+				titleData: text
 			},
 		})
-        await wrapper.setData({showDialog: true});
-        expect(wrapper.find('.module-cancel-button').exists()).toBe(false)
+		await wrapper.setData({showDialog: true});
+		expect(wrapper.find('.module-cancel-button').exists()).toBe(false)
 
-        wrapper = mount(Dialog, {
+		wrapper = mount(Dialog, {
 			props: { 
-                searchField: 'true',
-                okButton: 'true',
-                cancelButton: 'true',
-                iconData: svgfile,
-                titleData: text
+				searchField: 'true',
+				okButton: 'true',
+				cancelButton: 'true',
+				iconData: svgfile,
+				titleData: text
 			},
 		})
-        await wrapper.setData({showDialog: true});
-        expect(wrapper.find('.module-cancel-button').exists()).toBe(true)
+		await wrapper.setData({showDialog: true});
+		expect(wrapper.find('.module-cancel-button').exists()).toBe(true)
 
-        await wrapper.find('.module-cancel-button').trigger('click')
-        expect(wrapper.emitted()).toHaveProperty('onCancel')
+		await wrapper.find('.module-cancel-button').trigger('click')
+		expect(wrapper.emitted()).toHaveProperty('onCancel')
 	});
 
-    it('searchInput emitted on input in searchBar when passed', async () => {
-        wrapper = mount(Dialog, {
+	it('searchInput emitted on input in searchBar when passed', async () => {
+		wrapper = mount(Dialog, {
 			props: { 
-                searchField: 'false',
-                okButton: 'true',
-                cancelButton: 'true',
-                iconData: svgfile,
-                titleData: text
+				searchField: 'false',
+				okButton: 'true',
+				cancelButton: 'true',
+				iconData: svgfile,
+				titleData: text
 			},
 		})
-        await wrapper.setData({showDialog: true});
-        expect(wrapper.find('.settings-filter-text').exists()).toBe(false)
-        
-        wrapper = mount(Dialog, {
+		await wrapper.setData({showDialog: true});
+		expect(wrapper.find('.settings-filter-text').exists()).toBe(false)
+		
+		wrapper = mount(Dialog, {
 			props: { 
-                searchField: 'true',
-                okButton: 'true',
-                cancelButton: 'true',
-                iconData: svgfile,
-                titleData: text
+				searchField: 'true',
+				okButton: 'true',
+				cancelButton: 'true',
+				iconData: svgfile,
+				titleData: text
 			},
 		})
-        await wrapper.setData({showDialog: true});
-        expect(wrapper.find('.settings-filter-text').exists()).toBe(true)
+		await wrapper.setData({showDialog: true});
+		expect(wrapper.find('.settings-filter-text').exists()).toBe(true)
 
-        let inputElement = wrapper.find('.settings-filter-text');
+		let inputElement = wrapper.find('.settings-filter-text');
 		await inputElement.trigger('input-changed', 'q')
-        
-        expect(wrapper.emitted().searchInput).toHaveLength(1)
-        let obj= wrapper.emitted().searchInput[0];
-        expect(Object.values(obj[0])[0]).toEqual('q');
+		
+		expect(wrapper.emitted().searchInput).toHaveLength(1)
+		let obj= wrapper.emitted().searchInput[0];
+		expect(Object.values(obj[0])[0]).toEqual('q');
 	});
 
-    it('if no prop value is passed then hide particular feature', async () => {
-        // searchField prop not passed
-        wrapper = mount(Dialog, {
+	it('if no prop value is passed then hide particular feature', async () => {
+		// searchField prop not passed
+		wrapper = mount(Dialog, {
 			props: { 
-                okButton: 'false',
-                cancelButton: 'true',
-                iconData: svgfile,
-                titleData: text
+				okButton: 'false',
+				cancelButton: 'true',
+				iconData: svgfile,
+				titleData: text
 			},
 		})
-        await wrapper.setData({showDialog: true});
-        expect(wrapper.find('.settings-filter-text').exists()).toBe(false)
-        expect(wrapper.find('.module-cancel-button').exists()).toBe(true)
-        expect(wrapper.find('.module-ok-button').exists()).toBe(false)
+		await wrapper.setData({showDialog: true});
+		expect(wrapper.find('.settings-filter-text').exists()).toBe(false)
+		expect(wrapper.find('.module-cancel-button').exists()).toBe(true)
+		expect(wrapper.find('.module-ok-button').exists()).toBe(false)
 	});
 })
